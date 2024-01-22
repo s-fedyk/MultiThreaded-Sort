a57dd22c12da77da5e6af9b7cb1ca979ca77ae90
sfedyk <stan.fedyk@truenorthmortgage.ca>
2024-01-22 12:02:43 -0700

    progress

--- main.cpp
+++ main.cpp
@@ -1,65 +1,37 @@
 #include <iostream>
 #include <mutex>
+#include "barrier.h"
 #include <cstdlib>
 #include <pthread.h>
 #include <pthread_impl.h>
 #include <sys/_pthread/_pthread_mutex_t.h>
+#include <signal.h>
+#include <sys/signal.h>
 
 #define DEBUG
 
 using namespace std;
 
-struct pthread_barrier {
-  int waitingCount;
-  int conditionCount;
-  pthread_mutex_t lock;
-  pthread_cond_t cond;
-};
-
-pthread_barrier mbarrier;
-
-int pthread_barrier_init(pthread_barrier * b, const int conditionCount) {
-
-  b->conditionCount = conditionCount;
-  b->waitingCount = 0;
-
-  pthread_mutex_init(&b->lock, 0);
-  pthread_cond_init(&b->cond, 0);
-
-  return 1;
-};
-
-int pthread_barrier_await(pthread_barrier * b) {
-  pthread_mutex_lock(&b->lock);
-  b->waitingCount += 1;
-
-  if (b->waitingCount >= b->conditionCount) {
-    pthread_cond_broadcast(&b->cond);
-    pthread_mutex_unlock(&b->lock);
-    return 0; // this was the thread that unlocked us
-  } else {
-    // give up lock and wait for condition
-    pthread_cond_wait(&b->cond, &b->lock);
-    pthread_mutex_unlock(&b->lock);
-  }
-
-  return 1;
-}
-
-int pthread_barrier_destroy(pthread_barrier * b) {
-  pthread_cond_destroy(&b->cond);
-  pthread_mutex_destroy(&b->lock);
-
-  return 1;
-}
-
 struct thread_job {
   int* list;
   size_t size;
+  //phase 1
+  int* regularSample;
 };
 
+pthread_barrier mbarrier;
+pthread_t* threads;
+
+// global job list
 thread_job **jobs;
-unsigned int numJobs;
+
+// your job
+thread_job *job;
+
+size_t n;
+size_t p;
+size_t w;
+
 int list[] = {73, 123, 13, 8, 9, 12, 7, 231, 39 ,2, 38, 39};
 
 int compare(const void *l, const void *r) {
@@ -68,17 +40,17 @@ int compare(const void *l, const void *r) {
 
 void phase1(int list[], size_t size) {
   qsort(list, size, sizeof(int), compare);
+
+  for (size_t i = 0 ; i < p ; i++) {
+
+  }
 }
 
 void* psrs(void* arg) {
 
   thread_job *job = (thread_job*)arg;
 
-#ifdef DEBUG
-  std::cout << "Thread initialized! Running psrs... size of " << job->size << std::endl;
-#endif // DEBUG
-  
-  phase1(job->list, job->size - 1);
+  phase1(job->list, job->size);
 
   pthread_barrier_await(&mbarrier);
 
@@ -86,37 +58,36 @@ void* psrs(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
-
-  numJobs = std::stoi(argv[1]);
-
-#ifdef DEBUG
-  std::cout << "Creating " <<  numJobs << " threads" << std::endl;
-#endif // DEBUG
+  n = sizeof(list)/sizeof(int);
+  p = std::stoi(argv[1]);
+  w = n/p^2;
 
   // + 1, main thread
-  pthread_barrier_init(&mbarrier, numJobs+1);
+  pthread_barrier_init(&mbarrier, p+1);
   
-  pthread_t threads[numJobs];
-  for (size_t i = 0; i < numJobs ; i++) {
+  pthread_t threads[p];
+  thread_job jobs[p];
 
-#ifdef DEBUG
-  std::cout << "Creating thread number " << i << std::endl;
-#endif // DEBUG
-    thread_job job;
+  size_t sampleSize = n/p;
 
-    job.size= (sizeof(list)/sizeof(int))/numJobs;
-    job.list = list + job.size * i;
-
-    pthread_create(&threads[i], NULL, &psrs, &job);
+  for (size_t i = 0; i < p ; i++) {
+    jobs[i].size = sampleSize;
+    jobs[i].list = list + jobs[i].size * i;
+    jobs[i].regularSample = new int[p];
+    
+    pthread_create(&threads[i], NULL, &psrs, &jobs[i]);
   }
 
-  cout << "Main thread waiting..." << std::endl;
   pthread_barrier_await(&mbarrier);
-  cout << "Main thread printing..." << std::endl;
 
-  for (size_t i = 0 ; i < sizeof(list)/sizeof(int) ; i ++) {
+  for (size_t i = 0 ; i < n ; i ++) {
     std::cout << list[i] << " ";
   }
+
+
+  for (size_t i = 0; i < p ; i++) {
+    free(jobs[i].regularSample);
+  }  
 
   pthread_barrier_destroy(&mbarrier);
   return 1;
